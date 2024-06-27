ccc11db07160e915895a450717b326f0ccb2700b
Christopher Rackauckas <accounts@chrisrackauckas.com>
2024-06-27 10:08:01 +0800

    Remove recompile_invalidations
    `@recompile_invalidations` should only be used in very specific scenarios, and this is not one of those scenarios. Also, there are big changes being done with https://github.com/SciML/CommonWorldInvalidations.jl. With that, we only need to `@recompile_invalidations` on a few entry points. In particular, Static.jl, Symbolics.jl, and preferably ForwardDiff.jl and StaticArrays.jl would adopt it too. But this means that in order to handle all of this effectively, in SciML we only need to apply it on Static.jl, Symbolics.jl, and SciMLBase.jl and the whole ecosystem should be fine.
    
    In any case, this library doesn't need it. It shouldn't make a tangible difference in compile times, while it increases precompile times by a lot.

--- src/NonlinearSolveBase.jl
+++ src/NonlinearSolveBase.jl
@@ -1,15 +1,11 @@
 module NonlinearSolveBase
 
-import PrecompileTools: @recompile_invalidations
-
-@recompile_invalidations begin
-    import ArrayInterface, SciMLBase, StaticArraysCore
-    import ConcreteStructs: @concrete
-    import FastClosures: @closure
-    import LinearAlgebra: norm
-    import Markdown: @doc_str
-    import SciMLBase: ReturnCode
-end
+import ArrayInterface, SciMLBase, StaticArraysCore
+import ConcreteStructs: @concrete
+import FastClosures: @closure
+import LinearAlgebra: norm
+import Markdown: @doc_str
+import SciMLBase: ReturnCode
 
 include("common_defaults.jl")
 include("termination_conditions.jl")
